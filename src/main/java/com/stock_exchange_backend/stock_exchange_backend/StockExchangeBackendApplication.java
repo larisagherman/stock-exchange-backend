e527ea80dead75ca99f054f960fa173079216b7c
Gherman Larisa <gherman.larisa3@gmail.com>
2025-11-26 14:16:00 +0200

    initialised springboot

---
+++ src/main/java/com/stock_exchange_backend/stock_exchange_backend/StockExchangeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stock_exchange_backend.stock_exchange_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockExchangeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockExchangeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stock_exchange_backend/stock_exchange_backend/StockExchangeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock_exchange_backend.stock_exchange_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockExchangeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
